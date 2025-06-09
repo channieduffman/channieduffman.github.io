aad4dc42d4f4195e62905219d034c07299da2b1e
Chandler Duffey <chandlerduffey@gmail.com>
2025-06-08 20:50:49 -0500

    add brown theme

--- js/app.js
+++ js/app.js
@@ -1,5 +1,5 @@
 function changeStyle(theme) {
-    const validThemes = ["dark-theme", "light-theme", "red-theme"];
+    const validThemes = ["dark-theme", "light-theme", "red-theme", "brown-theme"];
     let htmlElem = document.getElementById("root");
     if (validThemes.includes(theme)) {
         htmlElem.className = theme;
