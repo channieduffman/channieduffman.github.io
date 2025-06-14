8109307ce060c73d5c0deea6c12c1b561c1a03bc
Chandler Duffey <chandlerduffey@gmail.com>
2025-06-14 00:08:28 -0500

    change brown theme to high contrast

--- js/app.js
+++ js/app.js
@@ -1,5 +1,5 @@
 function changeStyle(theme) {
-    const validThemes = ["dark-theme", "light-theme", "red-theme", "brown-theme"];
+    const validThemes = ["high-contrast-theme", "dark-theme", "light-theme", "red-theme", "brown-theme"];
     let htmlElem = document.getElementById("root");
     if (validThemes.includes(theme)) {
         htmlElem.className = theme;
